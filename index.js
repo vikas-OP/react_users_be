9fb2cef8ac0a66e1e54f546c10ec3d0efa26e47f
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-09-27 13:46:37 +0530

    testing

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ const URL = "mongodb://localhost:27017";
 
 app.get("/users", async (req, res) => {
   try {
+    console.log("cool");
     var client = await mongoClient.connect(URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log("here's the problem");
     let db = client.db("react_users");
     let usersData = await db
       .collection("users")
